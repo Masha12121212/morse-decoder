9ff31763ad2b8f1940fe071f7ec1f75b7d335726
Mariia Shaf <mashashef1212@gmail.com>
2022-02-01 22:58:03 +0200

    fix:morse

--- src/index.js
+++ src/index.js
@@ -35,12 +35,30 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '':' '
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  let arr = [];
+  let arr1 = [];
+  let obj = { '11' : '-', '10':'.', '**':''}
+  let b = '';
+  let n = '';
 
+  for(i=0; i<expr.length; i = i+10){
+    arr.push(expr.slice(i, i+10 ));}
+  for (let a of arr){
+      b = a;
+    for(i = 0; i < a.length; i++){
+if (b[0] === '0'){ b = b.substring(1)}; };
+for (u=0; u< b.length; u = u+2){
+  n = n + obj[b[u]+b[u+1]]}
+arr1.push(MORSE_TABLE[n]);
+    n = ''; }
+    let res = ''
+   for(let el of arr1){res = res + el }
+  return res;
+  }
 module.exports = {
-    decode
+    decode,
 }
